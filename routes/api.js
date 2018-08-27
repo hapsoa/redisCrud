4687d895dbfd980f7f775ecee34b287ff1242078
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2018-08-27 17:25:09 +0900

    "redis client"

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const redis = require('redis');
-// const client = redis.createClient();
+const redis = require('redis');
+const client = redis.createClient(6379, '127.0.0.1');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'API SERVER' });
 });
-
 
 module.exports = router;
